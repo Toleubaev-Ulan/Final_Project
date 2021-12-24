81bcd7fdc62350316d677fe78ab536e156f0eb09
sagyndev <91341217+sagyndev@users.noreply.github.com>
2021-12-24 20:50:43 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,44 @@
 var $hamburger = document.querySelector('.hamburger');
 var $menu = document.querySelector('.menu-bar');
-var $cartModal = document.querySelector('#card-modal');
-var $cartButton = document.querySelector('.header__menu--cart');
-var $cartClose = document.querySelector('#cart-modal__close');
+
+var email = document.forms['form']['email'];
+var password = document.forms['form']['password'];
+
+
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
+
+
+function validated(){
+    if(email.value.length < 9 ){
+        email.style.border = "1px solid red";
+        email.focus();
+        return false;
+    }
+    if(password.value.length < 6 ){
+        password.style.border = "1px solid red";
+        password.focus();
+        return false;
+    }
+}
+
+function email_Verify(){
+    if(email.value.length >= 8){
+        email.style.border = "1px solid silver";
+        return true;
+    }
+}
+
+function pass_Verify(){
+    if(password.value.length >= 6){
+        password.style.border = "1px solid silver";
+        return true;
+    }
+}
 
 function toggleMenu() {
     $menu.classList.toggle('active-mobile');
 }
-
-function toggleCart() {
-    $cartModal.classList.toggle('cart-modal--on');
-}
-
-function hideCart() {
-    $cartModal.classList.remove('cart-modal--on');
-}
-
 $hamburger.addEventListener('click', toggleMenu);
-$cartButton.addEventListener('click', toggleCart);
-$cartClose.addEventListener('click', hideCart);
+
 
